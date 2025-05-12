965ddfaaa184532e7f47efe9f575d4a32d4b7342
lucasmessias91 <lmessias91@gmail.com>
2025-05-12 19:03:51 -0300

    add styled-components globals

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import GlobalStyles from './styles/globalStyles' 
 
 import Router from './routes/routes.jsx'
 
@@ -9,5 +10,6 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Router />
     </BrowserRouter>
+    <GlobalStyles />
   </StrictMode>,
 )
