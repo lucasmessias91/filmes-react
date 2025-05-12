d62d36d08e474cb7f564659c324030359e2b619a
lucasmessias91 <lmessias91@gmail.com>
2025-05-12 19:44:55 -0300

    add useState

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,14 +1,27 @@
 import api from "../../services/api";
+import { Background } from "./styles";
+import { useState } from "react";
+
 function Home() {
+  const [movie, setMovie] = useState();
+
   async function getMovies() {
     const data = await api.get("/movie/popular");
+
+    setMovie(data.data.results[1]);
   }
 
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Essa é a Home</p>
-    </div>
+    <>
+      {movie && (
+        <div>
+          <Background img="https://image.tmdb.org/t/p/w500/8j0v2q3x1c4g5k7z6e4f3g3g3g3.jpg">
+            <h1>{movie.title}</h1>
+            <p>{movie.overview}</p>
+          </Background>
+        </div>
+      )}
+    </>
   );
 }
 
