23f4fff4fcbb37a86a6dc0d1e972a5a0eba9c7e8
lucasmessias91 <lmessias91@gmail.com>
2025-05-12 22:39:48 -0300

    fix useState

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,21 +1,24 @@
 import api from "../../services/api";
 import { Background } from "./styles";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Home() {
   const [movie, setMovie] = useState();
 
-  async function getMovies() {
-    const data = await api.get("/movie/popular");
-
-    setMovie(data.data.results[1]);
-  }
+  useEffect(() => {
+    async function getMovies() {
+      const { data } = await api.get("/movie/popular");
+      setMovie(data.results[1]);
+      console.log(data.results[1]);
+    }
+    getMovies();
+  }, []);
 
   return (
     <>
       {movie && (
         <div>
-          <Background img="https://image.tmdb.org/t/p/w500/8j0v2q3x1c4g5k7z6e4f3g3g3g3.jpg">
+          <Background img={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}>
             <h1>{movie.title}</h1>
             <p>{movie.overview}</p>
           </Background>
--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,17 @@
-import axios from "axios"
-import { languages } from "eslint-plugin-prettier"
- const api = axios.create({ 
-    baseURL: "https://api.themoviedb.org/3/",
-    params: {
-        api_key: "19f45697a27ec2a485a7669c2d214909",
-        language: "pt-BR",
-        page: 1
-    }
- })
+import axios from "axios";
 
- export default api
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9eyJhdWQiOiIxOWY0NTY5N2EyN2VjMmE0ODVhNzY2OWMyZDIxNDkwOSIsIm5iZiI6MTc0NjIzMTQwMC43MjQsInN1YiI6IjY4MTU2MDY4ODY3ZTUyMmFlZWJlOGYzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fDM1fkMRdRuR9ZkVo6luZ28gtWma5e8GkdwX0S7vuvY",
+    Accept: "application/json",
+  },
+  params: {
+    api_key: "19f45697a27ec2a485a7669c2d214909",
+    language: "pt-BR",
+    page: 1,
+  },
+});
+
+export default api;
