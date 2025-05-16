9ec44875ce3cb2bbd01dca892c7cc4f72c4e7b9d
lucasmessias91 <lmessias91@gmail.com>
2025-05-16 20:09:50 -0300

    style: card e slider

--- src/components/Slider/styles.js
+++ src/components/Slider/styles.js
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  background-color: #000000;
+  padding: 0 20px;
+  h2 {
+    color: #ffffff;
+    margin: 20px 0 20px 20px;
+    font-size: 24px;
+    font-weight: 600;
+  }
+  .swiper-wrapper {
+    display: flex;
+  }
 `
   
 
