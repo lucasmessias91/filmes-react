c79b5406a4a73c84dcac7b9b3bfaf035ab355a57
lucasmessias91 <lmessias91@gmail.com>
2025-05-21 19:26:59 -0300

    feat: add Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,24 @@
-import Logo from "../../assets/logo1.png";
+import { Link } from "react-router-dom";
+import { Container, Li, Menu } from "./styles";
+import logo from "../../assets/logo1.png";
+
 
 function Header() {
   return (
-    <header>
-      <img src={Logo} alt="logo-react-filmes" style={{width: 300}}/>
-    </header>
+    <Container>
+      <img src={logo} alt="" />
+      <Menu>
+        <Li>
+          <Link to="/filmes-react/">Home</Link>
+        </Li>
+        <Li>
+          <Link to="/filmes-react/filmes">Filmes</Link>
+        </Li>
+        <Li>
+          <Link to="/filmes-react/series">Series</Link>
+        </Li>
+      </Menu>
+    </Container>
   );
 }
 
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 
+
 function DefaultLayout() {
   return (
     <>
-{/*       <Header /> */}
+      <Header />
       <Outlet />
     </>
   );
